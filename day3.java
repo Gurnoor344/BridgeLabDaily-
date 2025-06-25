f68623d154d592fd87f068010476ce5fbd3f79c4
gurnoor <gurnoor344.be22@chitkara.edu.in>
2025-06-25 15:29:40 +0530

    first commit

---
+++ day3.java
@@ -0,0 +1,21 @@
+public class day3 {
+    public static void main(String[] args){
+            System.out.println("HIIIIIIIIIII");
+
+            int a=10;
+            int b=30;
+            int c=a+b;
+            System.out.println(c);
+
+            int d=a-b;
+            System.out.println(d);
+
+            int e=a/b;
+            System.out.println(e);
+
+            int f=a*b;
+            System.out.println(f);
+
+        }
+
+}
