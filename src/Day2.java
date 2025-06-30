53bd884e94f3096fef2714b6adddbe591107bfd3
gurnoor <gurnoor344.be22@chitkara.edu.in>
2025-06-30 12:39:48 +0530

    fourth Commit

--- src/Day2.java
+++ src/Day2.java
@@ -1,2 +1,10 @@
-package src;public class Day2 {
+package src;
+
+public class Day2 {
+    public static void main(String[] args) {
+        double celsius =10.0 , fahren =0.0;
+
+        fahren=(celsius * 1.8 )+32;
+        System.out.println("value of fahrenheit : " + fahren);
+    }
 }
